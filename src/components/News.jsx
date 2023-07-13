f6b85e96f6cec650cb07dcf2960954a39d27e1df
Bikram48 <chandbikram001@gmail.com>
2023-07-13 11:18:55 +0545

    feature - search news using query

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -4,6 +4,8 @@ import ReactPaginate from "react-paginate";
 
 export default function News() {
   const [news, setNews] = useState([]);
+  const [search, setSearch] = useState("");
+
   useEffect(() => {
     async function fetchData() {
       const request = await fetch(
@@ -33,11 +35,33 @@ export default function News() {
     fetchNews(currentPage);
   };
 
+  useEffect(() => {
+    async function fetchData() {
+      const request = await fetch(
+        `https://newsapi.org/v2/everything?q=${search}&sources=bbc-news&pageSize=6&apiKey=5235a3e37c4f470ca8af56361d4f8148`
+      );
+      const resp = await request.json();
+      const respData = resp.articles;
+      setNews(respData);
+    }
+
+    fetchData();
+  }, [search]);
+
   return (
     <div className="max-w-4xl mx-auto mt-20">
       <div className="flex items-center gap-2">
         <div className="w-1 h-5 bg-black"></div>
         <h2 className="text-2xl">News</h2>
+        <form className="ml-auto" action="">
+          <input
+            className="outline-0"
+            type="text"
+            placeholder="Search News..."
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        </form>
       </div>
       <div className="grid grid-cols-3 gap-4 mt-6">
         {news.map((item) => (
