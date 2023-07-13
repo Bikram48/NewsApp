68a4457e58382ed91d4e193999c8fe347edf752a
Bikram48 <chandbikram001@gmail.com>
2023-07-13 11:00:57 +0545

    add - pagination

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,29 +1,67 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import NewsCard from "./NewsCard";
+import ReactPaginate from "react-paginate";
 
 export default function News() {
+  const [news, setNews] = useState([]);
   useEffect(() => {
     async function fetchData() {
       const request = await fetch(
-        "https://newsapi.org/v2/everything?sources=bbc-news&apiKey=1d95c2e165524a658c24a34fb01a4565"
+        "https://newsapi.org/v2/everything?sources=bbc-news&pageSize=6&apiKey=5235a3e37c4f470ca8af56361d4f8148"
       );
       const resp = await request.json();
       const respData = resp.articles;
+      setNews(respData);
     }
 
     fetchData();
   }, []);
+
+  const fetchNews = async (currentPage) => {
+    const request = await fetch(
+      `https://newsapi.org/v2/everything?sources=bbc-news&page=${currentPage}&pageSize=6&apiKey=5235a3e37c4f470ca8af56361d4f8148`
+    );
+    const resp = await request.json();
+    const respData = resp.articles;
+    console.log(respData);
+    setNews(respData);
+  };
+
+  const handlePageClick = (event) => {
+    console.log(event.selected);
+    let currentPage = event.selected + 1;
+    fetchNews(currentPage);
+  };
+
   return (
     <div className="max-w-4xl mx-auto mt-20">
       <div className="flex items-center gap-2">
         <div className="w-1 h-5 bg-black"></div>
         <h2 className="text-2xl">News</h2>
       </div>
-      <div className="flex gap-4 mt-6">
-        <NewsCard />
-        <NewsCard />
-        <NewsCard />
+      <div className="grid grid-cols-3 gap-4 mt-6">
+        {news.map((item) => (
+          <NewsCard
+            image={item.urlToImage}
+            title={item.title}
+            description={item.description}
+            url={item.url}
+          />
+        ))}
       </div>
+      <ReactPaginate
+        previousLabel={"<"}
+        nextLabel={">"}
+        breakLabel={"..."}
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={5}
+        pageCount={10}
+        activeClassName={"pagination_active"}
+        nextLinkClassName={"next_page"}
+        previousLinkClassName={"previous_page"}
+        disabledClassName={"pagination_disabled"}
+        className="mx-auto text-center flex flex-row gap-4 mb-8 mt-8 justify-center"
+      />
     </div>
   );
 }
--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -1,23 +1,14 @@
 import React from "react";
 
-export default function NewsCard() {
+export default function NewsCard({ image, title, description, url }) {
   return (
-    <div className="basis-2/6  shadow-lg shadow-gray-500/50">
+    <div className=" shadow-lg shadow-gray-500/50">
       <div className="w-full">
-        <img
-          src="https://source.unsplash.com/user/c_v_r/1900x800"
-          alt=""
-          className="rounded-t-md h-56 object-cover"
-        />
+        <img src={image} alt="" className="rounded-t-md h-56 object-cover" />
       </div>
       <div className="p-4">
-        <h1 className="text-lg font-bold">
-          Rushdie has 'crazy dreams' about stabbing attack
-        </h1>
-        <p className="mt-2">
-          Apple advertises its newest iPhone as having the brightest screen yet,
-          but when you use it, you prob...
-        </p>
+        <h1 className="text-lg font-bold">{title}</h1>
+        <p className="mt-2">{description}</p>
       </div>
       <button className="w-full bg-orange-500 p-2 rounded-b-md">
         Read More
